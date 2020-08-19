3d03bdd615eb20c51c0d1c81154e9bee1208d63b
lengocdiep123 <50695785+lengocdiep123@users.noreply.github.com>
2020-08-19 22:51:32 +0700

    19082020
    -Generate DB SQL server
    -Create Controller and View: Cong ty, chuyen, thiet bi, cum thiet bi, thong so ky thuat, lich su, ke hoach bao tri

---
+++ TDAKyThuatSource/TDAKyThuatSource/Areas/Admin/Controllers/ChuyenSanXuatController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TDAKyThuatSource.Areas.Admin.Controllers
+{
+    public class ChuyenSanXuatController : Controller
+    {
+        //
+        // GET: /Admin/ChuyenSanXuat/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ TDAKyThuatSource/TDAKyThuatSource/Areas/Admin/Controllers/KeHoachBaoTriController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TDAKyThuatSource.Areas.Admin.Controllers
+{
+    public class KeHoachBaoTriController : Controller
+    {
+        //
+        // GET: /Admin/KeHoachBaoTri/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
