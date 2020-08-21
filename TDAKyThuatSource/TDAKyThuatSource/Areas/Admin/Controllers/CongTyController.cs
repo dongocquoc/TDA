09aa76d177377208f6be9c7d7aa2c1c0e91777c7
lengocdiep123 <50695785+lengocdiep123@users.noreply.github.com>
2020-08-21 23:28:07 +0700

    21082020
    -create model conect database
    -create them,xoa,sua don vi

--- TDAKyThuatSource/TDAKyThuatSource/Areas/Admin/Controllers/CongTyController.cs
+++ TDAKyThuatSource/TDAKyThuatSource/Areas/Admin/Controllers/CongTyController.cs
@@ -1,18 +1,76 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TDAKyThuatSource.Models;
 
 namespace TDAKyThuatSource.Areas.Admin.Controllers
 {
     public class CongTyController : Controller
     {
+        private TDAKTEntities _context = new TDAKTEntities();
         //
         // GET: /Admin/CongTy/
         public ActionResult Index()
         {
             return View();
         }
-	}
+
+        [HttpGet]
+        public JsonResult ListCongty()
+        {
+            var ListCT = _context.DonVi.Select(s => new {s.iMaDonVi,s.sTenDonVi,s.sDiaChi }).ToList();
+            var list = JsonConvert.SerializeObject(ListCT,
+                       Formatting.None,
+                       new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            });
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult AddCongty(DonVi model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            else
+            {
+                _context.DonVi.Add(model);
+                _context.SaveChanges();
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult EditCongty(DonVi model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            else
+            {
+                _context.SaveChanges();
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult DeleteCongty(int id)
+        {
+            var model = _context.DonVi.Find(id);
+            if (model != null)
+            {
+                _context.DonVi.Remove(model);
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            return null;
+        }
+
+    }
 }
---
+++ TDAKyThuatSource/TDAKyThuatSource/Controllers/CongTyAdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TDAKyThuatSource.Models;
+
+namespace TDAKyThuatSource.Controllers
+{
+    public class CongTyAdminController : Controller
+    {
+        private TDAKTEntities _context = new TDAKTEntities();
+        //
+        // GET: /CongTy/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListCongty()
+        {
+            var ListCT = _context.DonVi.ToList();
+            return Json(ListCT, JsonRequestBehavior.AllowGet);
+        }
+	}
+}
